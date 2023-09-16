dba8a66a8072945ffaee8bb4768c1fd3f2b0ced0
MartinNicolasAlomo <alomomartin.27@gmail.com>
2023-09-16 19:22:58 -0300

    Primer Commit - testeo

---
+++ New SYSACAD/Testeo Sistema/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Testeo_Sistema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Testeo Commit Github!!!!");
+        }
+    }
+}
